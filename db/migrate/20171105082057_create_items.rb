5f4cd24006da6e75555b4f02a34b8c6f29c06750
gomesuit <gomesuit@gmail.com>
2017-11-07 02:10:36 +0900

    not null

--- db/migrate/20171105082057_create_items.rb
+++ db/migrate/20171105082057_create_items.rb
@@ -1,15 +1,15 @@
 class CreateItems < ActiveRecord::Migration[5.1]
   def change
     create_table :items do |t|
-      t.string :title
-      t.string :detail_page_url
-      t.string :asin
+      t.string :title, null: false
+      t.string :detail_page_url, null: false
+      t.string :asin, null: false
       t.string :small_image
       t.string :medium_image
       t.string :large_image
-      t.references :author
-      t.date :publication_date
-      t.string :introduction
+      t.references :author, null: false
+      t.date :publication_date, null: false
+      t.string :introduction, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20171106165809) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "detail_page_url", null: false
+    t.string "asin", null: false
+    t.string "small_image"
+    t.string "medium_image"
+    t.string "large_image"
+    t.integer "author_id", null: false
+    t.date "publication_date", null: false
+    t.string "introduction", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_items_on_author_id"
+  end
 
 end
