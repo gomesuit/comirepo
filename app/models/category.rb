4344b9a54a6400be3a6ce4a4008c1cd5009b1e4b
gomesuit <gomesuit@gmail.com>
2017-11-08 00:58:09 +0900

    annotate

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id             :integer          not null, primary key
+#  name           :string           not null
+#  browse_node_id :string           not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 class Category < ApplicationRecord
   has_many :item_categories, dependent: :destroy
   has_many :items, through: :item_categories
--- app/models/item_category.rb
+++ app/models/item_category.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: item_categories
+#
+#  id          :integer          not null, primary key
+#  item_id     :integer          not null
+#  category_id :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class ItemCategory < ApplicationRecord
   belongs_to :item
   belongs_to :category
