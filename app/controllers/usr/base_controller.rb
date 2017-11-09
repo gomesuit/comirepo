a8c60bc7fbc36ecc1877a38fd4a1e910900b8cad
gomesuit <gomesuit@gmail.com>
2017-11-10 02:52:52 +0900

    variant

--- app/controllers/usr/base_controller.rb
+++ app/controllers/usr/base_controller.rb
@@ -1,3 +1,10 @@
 class Usr::BaseController < ApplicationController
+  before_action :detect_device_variant
   layout 'usr/application'
+
+  private
+    def detect_device_variant
+      request.variant = :pc
+      request.variant = :sp if request.user_agent&.match?(/iPhone|iPod|Android/)
+    end
 end
