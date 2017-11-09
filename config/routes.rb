89dca749bd6111636b35a3d4332a975c2b8c0874
gomesuit <gomesuit@gmail.com>
2017-11-10 02:05:26 +0900

    view 整理

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   scope module: :usr do
     root 'items#index'
 
-    resources :items
+    resources :items, only: %w[index show]
   end
 
   scope module: :adm, as: :admin, path: :admin do
