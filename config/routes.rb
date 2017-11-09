342df7ff4729618da2af1ffd8f4a700fbcad752a
gomesuit <gomesuit@gmail.com>
2017-11-10 01:52:24 +0900

    adm

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
   resources :items
   resources :authors
   resources :categories
+
+  scope module: :usr do
+    root 'top#index'
+
+  end
+
+  scope module: :adm, as: :admin, path: :admin do
+    root 'items#index'
+
+    resources :items
+    resources :authors
+    resources :categories
+  end
 end
---
+++ test/controllers/adm/categories_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Adm::CategoriesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
