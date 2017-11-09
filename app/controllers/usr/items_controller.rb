7a13f822388cec0ff3c3ec85afd2a0780afcabb4
gomesuit <gomesuit@gmail.com>
2017-11-10 02:32:31 +0900

    bug fix

--- app/controllers/usr/items_controller.rb
+++ app/controllers/usr/items_controller.rb
@@ -3,11 +3,6 @@ class Usr::ItemsController < Usr::BaseController
   # GET /items.json
   def index
     @days = Item.order(publication_date: :asc).pluck('DISTINCT publication_date')
-    @items = Item.all.order(publication_date: :desc)
-
-    if params[:test]
-      render template: 'items/index2'
-    end
   end
 
   # GET /items/1
