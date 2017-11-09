3ef3941e3e082b50b3e5f47aaae8f866117df7c0
gomesuit <gomesuit@gmail.com>
2017-11-10 03:54:43 +0900

    meta tag

--- app/helpers/usr/base_helper.rb
+++ app/helpers/usr/base_helper.rb
@@ -1,2 +1,27 @@
 module Usr::BaseHelper
+  def default_meta_tags
+    {
+      site: Settings.site.default.name,
+      reverse: true,
+      title: Settings.site.default.title,
+      description: Settings.site.default.description,
+      keywords: Settings.site.default.keywords,
+      og: {
+        title: :title,
+        type: 'article',
+        url: request.original_url,
+        image: "#{root_url}#{asset_path('noimage_m.png')}",
+        site_name: Settings.site.default.name,
+        description: :description,
+        locale: 'ja_JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@comirepo',
+        title: :title,
+        description: :description,
+        image: "#{root_url}#{asset_path('noimage_m.png')}"
+      }
+    }
+  end
 end
