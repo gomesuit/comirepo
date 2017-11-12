32c8f40f80bfa3235026e174a593a5f28d7a317d
gomesuit <gomesuit@gmail.com>
2017-11-13 03:27:01 +0900

    404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception, except: :render_404
+
+  if !Rails.env.development?
+    rescue_from ActionView::MissingTemplate, with: :render_404
+    rescue_from ActiveRecord::RecordNotFound, with: :render_404
+    rescue_from ActionController::RoutingError, with: :render_404
+    rescue_from Exception, with: :render_404
+  end
+
+  def render_404
+    respond_to do |format|
+      format.html { render 'errors/404', status: 404 }
+      format.all { head :not_found }
+    end
+  end
 end
