35780306c62e0ecfdfdcf178980e64cc9b4246e1
gomesuit <gomesuit@gmail.com>
2017-11-13 02:25:11 +0900

    params対応

--- app/controllers/usr/items_controller.rb
+++ app/controllers/usr/items_controller.rb
@@ -2,12 +2,23 @@ class Usr::ItemsController < Usr::BaseController
   # GET /items
   # GET /items.json
   def index
-    @days = Item.where('publication_date >= ?', Date.today)
-                .order(publication_date: :asc)
-                .pluck('DISTINCT publication_date')
-    #@days = Item.where(publication_date: Date.yesterday..Date.tomorrow)
-    #            .order(publication_date: :asc)
-    #            .pluck('DISTINCT publication_date')
+    @days = Item
+
+    case params['tab']
+    when 'yesterday' then
+    when 'today' then
+      @days = @days.where(publication_date: Date.today)
+    when 'tomorrow' then
+    when 'thisweek' then
+      @days = @days.where(publication_date: Date.today.beginning_of_week..Date.today.end_of_week)
+    when 'nextweek' then
+      @days = @days.where(publication_date: Date.today.next_week.beginning_of_week..Date.today.next_week.end_of_week)
+    else
+      @days = @days.where('publication_date >= ?', Date.today)
+    end
+
+    @days = @days.order(publication_date: :asc)
+                 .pluck('DISTINCT publication_date')
   end
 
   # GET /items/1
