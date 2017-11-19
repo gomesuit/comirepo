f075bba4ea5879e6041c2aaf4f61c9b018930fc0
gomesuit <gomesuit@gmail.com>
2017-11-20 03:29:07 +0900

    basic auth

--- app/controllers/adm/base_controller.rb
+++ app/controllers/adm/base_controller.rb
@@ -1,3 +1,7 @@
 class Adm::BaseController < ApplicationController
   layout 'adm/application'
+
+  if ENV['BASIC_AUTH_USERNAME'].present? && ENV['BASIC_AUTH_PASSWORD'].present?
+    http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
+  end
 end
