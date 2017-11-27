2e88d71440315c29258ddd7f9db769af34d4e01e
gomesuit <gomesuit@gmail.com>
2017-11-27 23:16:16 +0900

    リファクタリング

--- app/controllers/adm/items_controller.rb
+++ app/controllers/adm/items_controller.rb
@@ -50,7 +50,7 @@ class Adm::ItemsController < Adm::BaseController
 
   def ecs
     UpdateItemWorker.perform_async [@item.asin]
-    redirect_to admin_item_url(@item), notice: '更新Jobの追加が正常終了しました。'
+    redirect_to edit_admin_item_url(@item), notice: '更新Jobの追加が正常終了しました。'
   end
 
   private
