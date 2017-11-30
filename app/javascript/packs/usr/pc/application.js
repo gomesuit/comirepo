98ea090b69ea09d1cf9dd2b2b34e16d26a6f1006
gomesuit <gomesuit@gmail.com>
2017-12-01 04:14:09 +0900

    sidekiq

--- app/javascript/packs/usr/pc/application.js
+++ app/javascript/packs/usr/pc/application.js
@@ -5,3 +5,22 @@ import 'src/usr/pc/items_slider.scss'
 
 // js
 import 'bootstrap/dist/js/bootstrap.min'
+var itemsSwiper = new Swiper ('.swiper-container', {
+  slidesPerView: 5.5,
+  spaceBetween: 10,
+  slidesPerColumn: 2,
+  //centeredSlides: true,
+  //loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
--- app/javascript/packs/usr/sp/application.js
+++ app/javascript/packs/usr/sp/application.js
@@ -1,6 +1,27 @@
 // css
-import 'src/swiper'
 import 'src/bootstrap'
+import 'src/swiper'
+import 'src/usr/sp/items_slider.scss'
 
 // js
 import 'bootstrap/dist/js/bootstrap.min'
+var itemsSwiper = new Swiper ('.swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  slidesPerColumn: 2,
+  //centeredSlides: true,
+  //loop: true,
+  freeMode: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
