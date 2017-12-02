1f0daea869ff3b9a26a94bda8e4a4109f1e3c6d6
gomesuit <gomesuit@gmail.com>
2017-12-03 06:12:25 +0900

    bug fix

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -2,11 +2,11 @@ SitemapGenerator::Sitemap.default_host = ENV['SITE_URL']
 
 SitemapGenerator::Sitemap.create do
   add root_path
-  add yesterday_path
-  add today_path
-  add tomorrow_path
-  add thisweek_path
-  add nextweek_path
+  add yesterday_items_path
+  add today_items_path
+  add tomorrow_items_path
+  add thisweek_items_path
+  add nextweek_items_path
 
   Item.all.each do |item|
     add item_path(item), lastmod: item.updated_at
