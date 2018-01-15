adb1b3073d2e2ecc893364a3a7aecc7258896845
gomesuit <gomesuit@gmail.com>
2018-01-16 05:07:18 +0900

    jsリファクタリング

--- app/javascript/components/usr/items/items.pc.js
+++ app/javascript/components/usr/items/items.pc.js
@@ -1,1 +1,28 @@
 import './items.pc.scss'
+import Swiper from 'swiper/dist/js/swiper.min'
+
+var itemsSwiper = new Swiper ('.swiper-container', {
+  slidesPerView: 5.5,
+  spaceBetween: 10,
+  slidesPerColumn: 2,
+  //centeredSlides: true,
+  //loop: true,
+  //pagination: {
+  //  el: '.swiper-pagination',
+  //  clickable: true,
+  //},
+  //scrollbar: {
+  //  el: '.swiper-scrollbar',
+  //  draggable: true,
+  //},
+  //navigation: {
+  //  nextEl: '.swiper-button-next',
+  //  prevEl: '.swiper-button-prev',
+  //},
+})
+
+var topItemsSwiper = new Swiper ('.top-swiper-container', {
+  slidesPerView: 4.5,
+  spaceBetween: 10,
+  slidesPerColumn: 1,
+})
--- app/javascript/packs/usr/sp/application.js
+++ app/javascript/packs/usr/sp/application.js
@@ -1,32 +1,4 @@
 import 'lib/bootstrap'
 import 'lib/swiper'
-import Swiper from 'swiper/dist/js/swiper.min'
 import 'components/usr/items/items.sp'
 import 'components/usr/counter/counter.sp'
-
-var itemsSwiper = new Swiper ('.swiper-container', {
-  slidesPerView: 2.5,
-  spaceBetween: 10,
-  slidesPerColumn: 2,
-  //centeredSlides: true,
-  //loop: true,
-  freeMode: true,
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'progressbar',
-  },
-  scrollbar: {
-    el: '.swiper-scrollbar',
-    draggable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-})
-
-var topItemsSwiper = new Swiper ('.top-swiper-container', {
-  slidesPerView: 3.5,
-  spaceBetween: 10,
-  slidesPerColumn: 1,
-})
