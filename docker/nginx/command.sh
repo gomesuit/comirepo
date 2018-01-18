c436c55eb918357fe58d568bf1f827e930d4abcc
gomesuit <gomesuit@gmail.com>
2018-01-19 02:13:28 +0900

    nginx opti

--- docker/nginx/command.sh
+++ docker/nginx/command.sh
@@ -4,5 +4,6 @@ set -e
 sed -i -e "s/__RAILS_HOST__/$RAILS_HOST/" /etc/nginx/nginx.conf
 sed -i -e "s/__RAILS_PORT__/$RAILS_PORT/" /etc/nginx/nginx.conf
 sed -i -e "s/__S3_BUCKET__/$S3_BUCKET/" /etc/nginx/nginx.conf
+sed -i -e "s#__TARGET_DOMAIN__#$TARGET_DOMAIN#" /etc/nginx/nginx.conf
 
 nginx -g "daemon off;"
