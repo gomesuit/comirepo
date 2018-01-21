4a37227996af2c00afcbd88cdcad93944ebab551
gomesuit <gomesuit@gmail.com>
2018-01-22 03:27:44 +0900

    annotate

--- app/models/footer_ad.rb
+++ app/models/footer_ad.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: footer_ads
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)      not null
+#  dom        :text(65535)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class FooterAd < ApplicationRecord
 end
